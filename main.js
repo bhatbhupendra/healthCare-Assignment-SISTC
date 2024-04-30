92237f0dd52cafcb608e4b5a8bc6996a9cd075b6
Bhupendra Bhat <bhupendra420110bhat@gmail.com>
2024-05-01 03:38:49 +0545

    Updated: basic design

---
+++ main.js
@@ -0,0 +1,21 @@
+const parallax_1 = document.getElementById("parallax_1");
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax_1.style.backgroundPositionY = offset * 0.7 + "px";
+});
+
+document.getElementById("bmiForm").onsubmit = function () {
+  // Get input values
+  var weight = parseFloat(document.getElementById("weight").value);
+  var height = parseFloat(document.getElementById("height").value) / 100; // Convert height to meters
+
+  // Calculate BMI
+  var bmi = weight / (height * height);
+
+  // Display result
+  document.getElementById("reasultBMI").innerHTML =
+    "Your BMI is: " + bmi.toFixed(2);
+
+  return false; // Prevent form submission
+};
